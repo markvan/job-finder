313c1087c9b840f1b1f92f24b727435d5757f202
mbassmvh <markvh@manchester.ac.uk>
2018-12-15 22:27:55 +0000

    starting to parse html

--- src/conversions/read_url.py
+++ src/conversions/read_url.py
@@ -1,11 +1,16 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-with urllib.request.urlopen("http://www.python.org") as url:
+with urllib.request.urlopen("https://www.cwjobs.co.uk/jobs/contract/innovation/in-london?postedwithin=1") as url:
     s = url.read()
     print(s)
 
 soup = BeautifulSoup(s, 'html.parser')
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+out = ""
+
+for link in soup.find('div', class_="job-results").find_all('div', class_="job new "):
+    out += str(link.find('a'))
+    out += str(link.find('p', class_="job-intro"))
+
+print(out)
