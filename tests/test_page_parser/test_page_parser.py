c12f7382d7253dfc467f84ca6c751ac1c97012bd
mbassmvh <markvh@manchester.ac.uk>
2018-12-16 23:20:40 +0000

    more on monkeypatching /mocking

--- tests/test_page_parser/test_page_parser.py
+++ tests/test_page_parser/test_page_parser.py
@@ -1,11 +1,12 @@
 from tests.fixture_data.fixture_data_acccess import single_page
 from src.conversions.cw_job_matches import CWJobMatches
 
-#mock inside the job matcher class
-CWJobMatches.get_jobs = lambda self: "single_page"
+# monkeypatch in the job matcher class
+# https://stackoverflow.com/questions/5036920/mocking-out-methods-on-any-instance-of-a-python-class
+CWJobMatches.get_html = lambda self: single_page('would_be_self')
 
 
 def test_parse():
-    CWJobMatches
-    print(single_page(1))
-CWJobMatches
+    # print(single_page('would_be_self')) # just for seeing the mocking / monkeypatching
+    job_matcher = CWJobMatches("https://www.cwjobs.co.uk/jobs/contract/innovation/in-london?postedwithin=1")
+    print(job_matcher.get_html())
