438a5eb1a33e1c9257e2b1c258e98892e26afb6b
mbassmvh <markvh@manchester.ac.uk>
2018-12-16 21:13:33 +0000

    created some raw hrm result pages for fixtures, starting to create page parser class

--- src/conversions/read_url.py
+++ src/conversions/read_url.py
@@ -1,16 +1,25 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-with urllib.request.urlopen("https://www.cwjobs.co.uk/jobs/contract/innovation/in-london?postedwithin=1") as url:
-    s = url.read()
-    print(s)
 
-soup = BeautifulSoup(s, 'html.parser')
+class CWPages:
+    def __init__(self, url):
+        self.url = url
 
-out = ""
 
-for link in soup.find('div', class_="job-results").find_all('div', class_="job new "):
-    out += str(link.find('a'))
-    out += str(link.find('p', class_="job-intro"))
+    def get_page(self):
+        with urllib.request.urlopen(self.url) as url:
+            self.contents = url.read()
 
-print(out)
+    def get_page_results(self):
+        self.get_page()
+        soup = BeautifulSoup(self.contents, 'html.parser')
+        out = ""
+        for link in soup.find('div', class_="job-results").find_all('div', class_="job new "):
+            out += str(link.find('a'))
+            out += str(link.find('p', class_="job-intro"))
+        print(out)
+
+
+url_result = CWPages("https://www.cwjobs.co.uk/jobs/contract/innovation/in-london?postedwithin=1")
+url_result.get_page_results()
