9fe2b5626883b055f0ffba3a7e9f847fa4f53d65
mbassmvh <markvh@manchester.ac.uk>
2018-12-18 08:51:42 +0000

    added todo notes

--- src/application/cwjobmatches.py
+++ src/application/cwjobmatches.py
@@ -1,6 +1,12 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
+#TODO new sponsored jobs may not have class "job new" check on these
+
+#TODO start working with search string, not url, at least in next_job and get_all_jobs
+
+#TODO the current implementation of next job gets all pages of jobs at once via get_all_jobs
+#    it would be better to get pages of jobs as needed by next_job during its repeated invocations
 
 class CWJobMatches:
     # get page source at the url
@@ -10,7 +16,7 @@ class CWJobMatches:
         return page_source
 
     # returns 0 to many jobs from a page at the url
-    def get_jobs(self, url):
+    def get_jobs_from_page(self, url):
         soup = BeautifulSoup(self.get_html(url), 'html.parser')
         out = ""
         for link in soup.find('div', class_="job-results").find_all('div', class_="job new "):
@@ -30,8 +36,12 @@ class CWJobMatches:
         out = ""
         u = url
         while last_count != 0:
-            jobs = str(self.get_jobs(u))
+            jobs = str(self.get_jobs_from_page(u))
             out += jobs
             last_count = jobs.count('class="job new " id=')
             u = self._get_next_url(u)
         return BeautifulSoup(out, 'html.parser')
+
+    def next_job(self, url):
+        for link in self.get_all_jobs(url).find_all('div', class_="job new "):
+            yield link
--- tests/test_application/test_cwjobmatches.py
+++ tests/test_application/test_cwjobmatches.py
@@ -2,6 +2,8 @@ from tests.test_fixtures.fixture_data_acccess import file_name_from_url
 from tests.test_fixtures.fixture_data_acccess import page_source_from_file_name
 from src.application.cwjobmatches import CWJobMatches
 
+#TODO Introduce pytest.fixture
+
 
 # test that the right file names will be used to read from in responce to url page  query
 def test_file_names_from_url():
@@ -20,7 +22,7 @@ def create_job_matcher():
 
 # test that the CWJobMatches 'fetches from files' the right html for three urls
 # using CWJobMatches().get_html(self, url)
-# normally that instance method is called by instance method get_jobs(self, url)
+# normally that instance method is called by instance method get_jobs_from_page(self, url)
 def test_file_content_from_url():
     job_matcher = create_job_matcher()
     url = "https://www.cwjobs.co.uk/jobs/contract/innovation/in-london?postedwithin=1"
@@ -39,9 +41,9 @@ def _count_jobs(html_fragment):
 def test_num_of_jobs_from_url():
     job_matcher = create_job_matcher()
     url = "https://www.cwjobs.co.uk/jobs/contract/innovation/in-london?postedwithin=1"
-    assert _count_jobs(job_matcher.get_jobs(url)) == 17
+    assert _count_jobs(job_matcher.get_jobs_from_page(url)) == 17
     url = "https://www.cwjobs.co.uk/jobs/contract/innovation/in-london?postedwithin=1&page=3"
-    assert _count_jobs(job_matcher.get_jobs(url)) == 0
+    assert _count_jobs(job_matcher.get_jobs_from_page(url)) == 0
 
 
 def test_next_url():
@@ -60,5 +62,14 @@ def test_all_jobs():
     # the three pages have 17, 4 and 0 jobs on them
     assert _count_jobs(job_matcher.get_all_jobs(url)) == 17+4+0
 
+
+# could do with better test, only relying on counts
+def test_loop():
+    job_matcher = create_job_matcher()
+    url = "https://www.cwjobs.co.uk/jobs/contract/innovation/in-london?postedwithin=1"
+    count = 0
+    for job in job_matcher.next_job(url):
+        count += 1
+    assert count == 17+4+0
 
 
