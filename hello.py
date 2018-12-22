2ce9f9a36b9c8f8018a13469916a30cf66c2ffd0
mbassmvh <markvh@manchester.ac.uk>
2018-12-22 22:41:16 +0000

    starting to look reasonable, contract jobs only, next to be able to select what to look for

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from src.application.cwjobmatches import CWJobMatches
+from src.application.cwjobspagegetter import CWJobsPageGetter
 from tests.test_fixtures.fixture_data_acccess import page_source_from_file_name
 
 def create_job_matcher(production):
@@ -15,7 +16,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    url = "https://www.cwjobs.co.uk/jobs/contract/innovation/in-london?postedwithin=1"
+    url = "https://www.cwjobs.co.uk/jobs/contract/agile/in-london?postedwithin=1"
     production = True
     job_matcher = create_job_matcher(production)
     out = '''
@@ -23,12 +24,22 @@ def hello():
     <div class="job-container">
     '''
     for job in job_matcher.next_job(url):
-        out += ('<hr/>' + job)
+        out +=  job
     return out + " </div>"
 
 @app.route('/template')
 def home():
     return render_template('home.html')
+
+@app.route('/getjob/')
+def getjob():
+    print(request)
+    url = request.args.get('url')
+    r = CWJobsPageGetter.get(url)
+    r2 = r.replace(b'href="/', b'href="https://www.cwjobs.co.uk/')
+    r3 = r2.replace(b'src="/', b'src="https://www.cwjobs.co.uk/')
+    return r3
+
 
 
 if __name__ == "__main__":
--- src/application/jobTagSelection.py
+++ src/application/jobTagSelection.py
@@ -1,11 +1,62 @@
 from bs4 import BeautifulSoup
+from src.application.cwjobspagegetter  import CWJobsPageGetter
+
 
 class JobTagSelection:
-    @classmethod
-    def select(cls, job):
-        out = "<div class='job'>"
-        out += str(job.find('div', class_='job-title')).replace('<a ', '<a target="_blank" rel="noopener noreferrer" ')
-        out += str(job.find('p', class_='job-intro'))
-        out += "</div>"
-        return out.replace('...', "").replace('…',"").replace('  ', ' ').replace(" ", ' ')
+    @staticmethod
+    def select(job):
+        # build the job div
+        out = ""
+        if JobTagSelection.is_contract(job):
+            out = "<div class='job'>"
+            out += JobTagSelection.heading(job)
+            out += '<div class="notes">'
+            out += '<div class="textbox">'
+            out += '<p class="alignleft">' + JobTagSelection.salary(job) + '</p>'
+            out += '<p class="alignright">' + JobTagSelection.when(job) + '</p>'
+            out += '</div><div style="clear: both;"></div>'
+            out += '<p>' + JobTagSelection.location(job) + '</p>'
+            out += '</div>'
+            # out += '<p>&nbsp</p>'
+            out += str(JobTagSelection.job_description(job))
+            out += "</div>"
+            out = out.replace('...', "").replace('…',"").replace('  ', ' ').replace(" ", ' ')
+        return out
+
+    @staticmethod
+    def heading(job):
+        out = str(job.find('div', class_='job-title'))
+        out = out.replace('<a ', '<a target="_blank" rel="noopener noreferrer" ')
+        out = out.replace('<span class="label label-featured">Featured</span>', "")
+        out = out.replace('<span class="label label-featured">Premium</span>', "")
+        return out
+
+
+    @staticmethod
+    def job_description(job):
+        # fid the url for the job
+        url = job.find('div', class_='job-title').find('a')['href']
+        # get the job page html
+        job_page = CWJobsPageGetter.get(url)
+        soup = BeautifulSoup(job_page, 'html.parser')
+        # return the job description
+        return soup.find('div', class_='job-description')
+
+    @staticmethod
+    def is_contract(job):
+        job_type = str(job.find('li', class_='job-type').text)
+        return 'Contract' in job_type
+
+    @staticmethod
+    def salary(job):
+        return str(job.find('li', class_='salary').text)
+
+    @staticmethod
+    def location(job):
+        out = str(job.find('li', class_='location').text)
+        return out.replace('from', "").replace('- Update', "").replace('Update', "")
+
+    @staticmethod
+    def when(job):
+        return str(job.find('li', class_='date-posted').text)
 
