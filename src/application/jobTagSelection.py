7401b78223c69961640823811826a803d2de2f8c
mbassmvh <markvh@manchester.ac.uk>
2018-12-22 16:53:39 +0000

    just get up to date and push

--- src/application/jobTagSelection.py
+++ src/application/jobTagSelection.py
@@ -1,11 +1,11 @@
 from bs4 import BeautifulSoup
 
-
 class JobTagSelection:
     @classmethod
     def select(cls, job):
-        out = ""
-        out += str(job.find('div', class_='job-title'))
+        out = "<div class='job'>"
+        out += str(job.find('div', class_='job-title')).replace('<a ', '<a target="_blank" rel="noopener noreferrer" ')
         out += str(job.find('p', class_='job-intro'))
+        out += "</div>"
         return out.replace('...', "").replace('…',"").replace('  ', ' ').replace(" ", ' ')
 
