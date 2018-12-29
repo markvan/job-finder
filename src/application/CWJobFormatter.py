ab53d95f91fbcbbc16c760b0dda0fc07c9631512
mbassmvh <markvh@manchester.ac.uk>
2018-12-29 16:23:32 +0000

    added initial underscores to method names in CWJobFormatter

--- src/application/CWJobFormatter.py
+++ src/application/CWJobFormatter.py
@@ -7,17 +7,17 @@ class CWJobFormatter:
     def format(job):
         # build the job div
         out = ""
-        if CWJobFormatter.is_contract(job):
+        if CWJobFormatter._is_contract(job):
             out = "<div class='job'>"
-            out += CWJobFormatter.heading(job)
+            out += CWJobFormatter._heading(job)
             out += '<div class="notes">'
             out += '<div class="textbox">'
-            out += '<p class="alignleft">' + CWJobFormatter.salary(job) + '</p>'
-            out += '<p class="alignright">' + CWJobFormatter.date_posted(job) + '</p>'
+            out += '<p class="alignleft">' + CWJobFormatter._salary(job) + '</p>'
+            out += '<p class="alignright">' + CWJobFormatter._date_posted(job) + '</p>'
             out += '</div><div style="clear: both;"></div>'
-            out += '<p>' + CWJobFormatter.location(job) + '</p>'
+            out += '<p>' + CWJobFormatter._location(job) + '</p>'
             out += '</div>'
-            out += str(CWJobFormatter.description(job))
+            out += str(CWJobFormatter._description(job))
             out += "</div>"
             out = out.replace('...', "").replace('…',"").replace('  ', ' ').replace(" ", ' ')
             #TODO messy clean up
@@ -25,7 +25,7 @@ class CWJobFormatter:
         return out
 
     @staticmethod
-    def heading(job):
+    def _heading(job):
         out = str(job.find('div', class_='job-title'))
         out = out.replace('<a ', '<a target="_blank" rel="noopener noreferrer" ')
         out = out.replace('<span class="label label-featured">Featured</span>', "")
@@ -33,8 +33,10 @@ class CWJobFormatter:
         return out
 
 
+
+    # this finds the job description from the page at the url in the search results
     @staticmethod
-    def description(job):
+    def _description(job):
         # fid the url for the job
         url = job.find('div', class_='job-title').find('a')['href']
         # get the job page html
@@ -43,21 +45,22 @@ class CWJobFormatter:
         # return the job description
         return soup.find('div', class_='job-description')
 
+    # remaining methods just get info from the job description in the search results
     @staticmethod
-    def is_contract(job):
+    def _is_contract(job):
         job_type = str(job.find('li', class_='job-type').text)
         return 'Contract' in job_type
 
     @staticmethod
-    def salary(job):
+    def _salary(job):
         return str(job.find('li', class_='salary').text)
 
     @staticmethod
-    def location(job):
+    def _location(job):
         out = str(job.find('li', class_='location').text)
         return out.replace('from', "").replace('- Update', "").replace('Update', "")
 
     @staticmethod
-    def date_posted(job):
+    def _date_posted(job):
         return str(job.find('li', class_='date-posted').text)
 
